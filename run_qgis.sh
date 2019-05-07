4a8ef00ec46c11ef6f90caf87d0fc8da64a73c75
doublebyte1 <doublebyte@gmail.com>
2019-05-07 11:22:47 +0200

    - updated script

--- run_qgis.sh
+++ run_qgis.sh
@@ -4,6 +4,7 @@ xhost +
 docker run --rm -it --name qgis \
     -v /home/$USER/:/mnt \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /home/$USER/mnt_qgis:/root/.local/share/QGIS/QGIS3/
     -e DISPLAY=unix$DISPLAY \
     qgis/qgis:latest qgis
 xhost -
